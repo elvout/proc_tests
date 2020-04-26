3acf909a9d722cb14ea6e838c2bd2e01a7a0d19f
elvout <elvin.yang9@gmail.com>
2020-04-25 23:43:12 -0500

    change jmp1 to iterate 10 times

--- jmp1.s
+++ jmp1.s
@@ -1,13 +1,12 @@
 movl r1, L0
-movl r2, 3
+movl r2, 10
 movl r3, 1
 
-// for (int i = 3; i != 0; i--) print('B')
-    // kinda not really but it prints 'B' 3 times
+// prints 'B' 10 times
 L0:
-sub r2, r2, r3
+sub r2, r2, r3  // loc 0006
 movl r0, 66
 jnz r1, r2
 
-movl r0, 65
+movl r0, 65 // loc 000C
 movl r0, 10
